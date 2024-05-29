78d70fa962257e07c199cb050f7e5bad460f5ea3
rain <3100406687@qq.com>
2024-05-29 20:10:32 +0800

    style 解决样式不被压缩的问题

---
+++ src/components/Menu.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <router-link
+      class="item router-link"
+      to="/"
+    >
+      产品
+    </router-link>
+    <a
+      class="item"
+      to="https://www.zzxxtech.com/actives/intledu"
+      target="_blank"
+    >
+      社区
+    </a>
+    <router-link
+      class="item"
+      to="/study"
+    >
+      学习
+    </router-link>
+    <router-link
+      class="item"
+      to="/about"
+    >
+      about
+    </router-link>
+  </div>
+</template>
+<style scoped lang="scss">
+// 如果直接写样式，不会被压缩
+// 方案1 使用unocss的transformers
+.itemd {
+  @apply text-center text-xl py-2 px-4 cursor-pointer;
+}
+</style>
--- uno.config.ts
+++ uno.config.ts
@@ -2,8 +2,14 @@
 import { defineConfig } from 'unocss'
 import presetWind from '@unocss/preset-wind'
 import { presetIcons } from 'unocss'
+import transformerDirectives from '@unocss/transformer-directives'
 
 export default defineConfig({
   // ...UnoCSS options
-  presets: [presetWind(), presetIcons({ extraProperties: { display: 'inline-block' } })]
+  presets: [presetWind(), presetIcons({ extraProperties: { display: 'inline-block' } })],
+  transformers: [transformerDirectives()],
+  // 针对全局组件的样式 class="router-link"
+  shortcuts: {
+    'router-link': ' text-xl font-300 cursor-pointer px-8 hover:(text-teal-600)'
+  }
 })
